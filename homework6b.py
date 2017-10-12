99f8a4780ed232446ef6dbf9b9cfd6701e325560
Catie <catieo@umich.edu>
2017-10-12 09:22:26 -0400

    Completed Homework 6 Part B

--- homework6b.py
+++ homework6b.py
@@ -6,11 +6,18 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False 
 ctx.verify_mode = ssl.CERT_NONE
 
-url = "http://py4e-data.dr-chuck.net/known_by_Fikret.html"
+url = "http://py4e-data.dr-chuck.net/known_by_Caedyn.html"
 html = urlopen(url, context=ctx).read()
 
 soup = BeautifulSoup(html, "html.parser")
 
-tags = soup('a')
-for tag in tags:
-	print(tag.get('href', None))
+counter = 7
+
+while counter > 0:
+	tags = soup('a')
+	print(tags[17].get('href', None))
+
+	url = tags[17].get('href', None)
+	html = urlopen(url, context=ctx).read()
+	soup = BeautifulSoup(html, "html.parser")
+	counter -= 1
